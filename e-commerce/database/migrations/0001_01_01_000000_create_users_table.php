bdedaadef9f3dbaa97352cf4bf8e1b2f7fa7191c
aimerpante <aimerpante071102@gmail.com>
2025-04-02 11:04:32 +0800

    added the logo

--- e-commerce/database/migrations/0001_01_01_000000_create_users_table.php
+++ e-commerce/database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use function GuzzleHttp\default_ca_bundle;
 
 return new class extends Migration
 {
@@ -13,7 +14,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('lastname');
+            $table->string('address');
+            $table->integer('usertype');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
